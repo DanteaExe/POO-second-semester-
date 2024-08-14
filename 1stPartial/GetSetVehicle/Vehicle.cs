f97b3015b77493c9e85c978953d427700c4fb762
DanteaExe <jorgedantex@gmail.com>
2024-08-14 08:38:31 -0600

    first commit
    creation of first homework

---
+++ 1stPartial/GetSetVehicle/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace GetSetVehicle
+{
+    public class Vehicle
+    {
+        public string? Color { get; set; }
+        public int NumOfCylinders { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
