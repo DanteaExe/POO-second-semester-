933884b4c2815a607d0e200c50cebaad15c5d939
DanteaExe <jorgedantex@gmail.com>
2024-11-22 12:59:56 -0600

    node and double list classes made

---
+++ 3rdPartial/doublelistProject/DoubleList/DoubleList.cs
@@ -0,0 +1,68 @@
+﻿namespace DoubleList;
+using Node;
+
+public class DoubleList<T>
+{
+    private Node<T>? Start;
+    private Node<T>? End;
+    private int Count;
+
+    public DoubleList()
+    {
+        Start = null;
+        End = null;
+        Count = 0;
+    }
+
+    public void Add(T item)
+    {
+        Start ??= new Node<T>(item, Start, End);
+        
+        End!.next = new Node<T>(item, Start, End = End.next);
+        
+        Count++;
+    }
+
+    public void Remove(T item)
+    {
+        if (Start is null)
+        {
+            return;
+        }
+
+        if (Start.value!.Equals(item))
+        {
+            Start = Start.next;
+            Count--;
+            return;
+        }
+
+        Node<T>? current = Start;
+        while (current is not null)
+        {
+            if (current.next is null)
+            {
+                return;
+            }
+        }
+    }
+
+    public void ShowList()
+    {
+        if (Start is null)
+        {
+            WriteLine("The list is empty");
+        }
+
+        while (Start is not null)
+        {
+            WriteLine(Start.value);
+            Start = Start.next;
+        }
+    }
+
+    public int CountList()
+    {
+        return Count;
+    }
+}
---
+++ 3rdPartial/doublelistProject/Node/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace Node;
+
+public class Node<T>(T? value, Node<T>? next, Node<T>? previous)
+{
+    private T? Value = value;
+    private Node<T>? Next = next; 
+    private Node<T>? Previous = previous;
+
+    public T? value { get => Value; set => Value = value; }
+    public Node<T>? next { get => Next; set => Next = value; }
+    public Node<T>? previous { get => Previous; set => Previous = value; }
+}
