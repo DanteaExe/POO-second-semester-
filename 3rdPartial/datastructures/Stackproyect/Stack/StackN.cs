18878e1d03b4335ed3a14719bbe5e7dfe6cb4fdd
DanteaExe <jorgedantex@gmail.com>
2024-12-04 14:31:37 -0600

    stack made OwO

---
+++ 3rdPartial/datastructures/Stackproyect/Stack/StackN.cs
@@ -0,0 +1,84 @@
+﻿namespace Stack;
+using Node;
+
+public class StackN<T>
+{
+    private Node<T>? Start;
+    private Node<T>? End;
+    private int Count;
+
+    public StackN()
+    {
+        Start = null;
+        End = null;
+        Count = 0;
+    }
+
+    public void Clear()
+    {
+        WriteLine("The stack got dusted... as we are going to be turned... we burn...we live...and the end we die... like the stack, it just die...");
+        Start = null;
+        End = null;
+        Count = 0;
+    }
+
+    public void Push(T item)
+    {
+        Node<T> newNode = new(item, null, End);
+
+        if (Start is null)
+        {
+            Start = newNode;
+            End = newNode;
+        }
+        else
+        {
+            End!.next = newNode;
+            End = newNode;
+        }
+
+        Count++;
+    }
+
+    public void Pop()
+    {
+        if (Start is null)
+        {
+            WriteLine("The stack is empty");
+            return;
+        }
+
+        if (Start == End)
+        {
+            Start = null;
+            End = null;
+        }
+        else
+        {
+            Start = Start!.next;
+        }
+
+        Count--;
+    }
+
+    public void ShowStack()
+    {
+        if (Start is null)
+        {
+            WriteLine("The stack is empty");
+            return;
+        }
+
+        Node<T>? current = Start;
+        while (current is not null)
+        {
+            WriteLine(current.value);
+            current = current.next;
+        }
+    }
+
+    public int CountStack()
+    {
+        return Count;
+    }
+}
---
+++ 3rdPartial/datastructures/Stackproyect/Stackprogram/Program.cs
@@ -0,0 +1,27 @@
+﻿using Stack;
+
+StackN<int> stack = new();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+stack.Push(6);
+
+stack.ShowStack();
+WriteLine($"The stack has: {stack.CountStack()} items");
+
+stack.Pop();
+stack.Pop();
+stack.Pop();
+
+WriteLine();
+
+stack.ShowStack();
+WriteLine($"The stack has: {stack.CountStack()} items");
+
+
+stack.Clear();
+stack.ShowStack();
+WriteLine($"The stack has: {stack.CountStack()} items");
